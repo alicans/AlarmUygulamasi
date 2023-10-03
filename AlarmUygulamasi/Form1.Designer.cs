de657f2b3c44d0ab50c370404625135c146ec233
alicans <alican@outlook.com>
2023-10-03 22:58:42 +0300

    Add project files.

---
+++ AlarmUygulamasi/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AlarmUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dtpZaman = new DateTimePicker();
+            btnKur = new Button();
+            statusStrip1 = new StatusStrip();
+            lblDurum = new ToolStripStatusLabel();
+            timer1 = new System.Windows.Forms.Timer(components);
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dtpZaman
+            // 
+            dtpZaman.Format = DateTimePickerFormat.Time;
+            dtpZaman.Location = new Point(49, 47);
+            dtpZaman.Margin = new Padding(6);
+            dtpZaman.Name = "dtpZaman";
+            dtpZaman.Size = new Size(197, 39);
+            dtpZaman.TabIndex = 0;
+            // 
+            // btnKur
+            // 
+            btnKur.Location = new Point(49, 98);
+            btnKur.Margin = new Padding(6);
+            btnKur.Name = "btnKur";
+            btnKur.Size = new Size(197, 49);
+            btnKur.TabIndex = 1;
+            btnKur.Text = "Alarm Kur";
+            btnKur.UseVisualStyleBackColor = true;
+            btnKur.Click += btnKur_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lblDurum });
+            statusStrip1.Location = new Point(0, 188);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(306, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lblDurum
+            // 
+            lblDurum.Name = "lblDurum";
+            lblDurum.Size = new Size(108, 17);
+            lblDurum.Text = "Alarm kurulu değil.";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(306, 210);
+            Controls.Add(statusStrip1);
+            Controls.Add(btnKur);
+            Controls.Add(dtpZaman);
+            Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(6);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Alarm Uygulaması";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dtpZaman;
+        private Button btnKur;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lblDurum;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ AlarmUygulamasi/Form1.cs
@@ -0,0 +1,61 @@
+using System.Media;
+
+namespace AlarmUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        bool alarmKuruluMu = false;
+        DateTime kuruluZaman;
+
+        // Form1 ilk oluþurken çalýþan metot (1 defa çalýþýr baþta)
+        public Form1()
+        {
+            InitializeComponent();
+            dtpZaman.Value = DateTime.Now.AddSeconds(10);
+        }
+
+        private void btnKur_Click(object sender, EventArgs e)
+        {
+            DateTime hedef = dtpZaman.Value;
+
+            if (hedef < DateTime.Now)
+            {
+                MessageBox.Show("Geçmiþ bir zaman seçtiniz.");
+            }
+            else if (btnKur.Text == "Alarm Kur")
+            {
+                kuruluZaman = hedef;
+                alarmKuruluMu = true;
+                lblDurum.Text = $"{kuruluZaman} tarihine alarm kuruldu.";
+                dtpZaman.Enabled = false;
+                btnKur.Text = "Ýptal Et";
+            }
+            else
+            {
+                IptalEt();
+            }
+        }
+
+        void IptalEt()
+        {
+            kuruluZaman = DateTime.MinValue;
+            alarmKuruluMu = false;
+            lblDurum.Text = "Alarm kurulu deðil.";
+            dtpZaman.Enabled = true;
+            btnKur.Text = "Alarm Kur";
+        }
+
+        SoundPlayer soundPlayer = new SoundPlayer("alarm.wav");
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (alarmKuruluMu && kuruluZaman <= DateTime.Now)
+            {
+                IptalEt();
+                soundPlayer.Play();
+                MessageBox.Show("Alarmý Sustur");
+                soundPlayer.Stop();
+            }
+        }
+    }
+}
